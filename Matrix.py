bb536d81eb8d50742d4cbfa27a0f0df6c20d76c6
Mouzone <sunnyliu010@gmail.com>
2024-01-23 19:41:12 -0500

    init functions written

---
+++ Matrix.py
@@ -0,0 +1,17 @@
+class Matrix:
+    # empty matrix 
+    def init():
+        self.container = []
+    
+    def print():
+        return
+
+    # Arithmetic operations
+    def __add__(self, other):
+        return
+
+    def __sub__(self, other):
+        return 
+
+    def __mul__(self, other):
+        return 
