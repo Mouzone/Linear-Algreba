45a458f90e2c83836df50df0dfb4f1193c1c94da
Mouzone <sunnyliu010@gmail.com>
2024-02-13 20:11:10 -0500

    moved operations inside matrix class and file due to oop principles

--- linear_algebra/matrix/matrix.py
+++ linear_algebra/matrix/matrix.py
@@ -80,3 +80,77 @@ class Matrix:
             if not isinstance(user_input[col], int) and not isinstance(user_input[col], float):
                 return f"Must be all integers"
         return None
+
+    def check_before_operations(self, other):
+        if self.height != other.height:
+            raise MatrixInitializationError(f"Heights are incompatible")
+        if self.width != other.width:
+            raise MatrixInitializationError(f"Widths are incompatible")   
+                
+    def __add__(self, other):
+        self.check_before_operations(other)
+        result = [
+            [self.container[i][j] + other.container[i][j] for j in range(self.width)]
+            for i in range(self.height)
+        ]
+        return Matrix(result)
+
+    def __sub__(self, other):
+        self.check_before_operations(other)
+        result = [
+            [self.container[i][j] - other.container[i][j] for j in range(self.width)]
+            for i in range(self.height)
+        ]
+        return Matrix(result)
+
+    def check_mul(self, other):
+        if self.width != other.height:
+            raise MatrixInitializationError(f"Dimensions are incompatible")   
+
+    # write the case when it is a constant on either side
+    def __mul__(self, other):
+        # self and other are matrices
+        # matrix_1 row i_1 * matrix_2_transposed row i_1 ...
+        if isinstance(self, Matrix) and isinstance(other, Matrix):
+            self.check_mul(other)
+            other_transpose = other.transpose()
+
+            result = [[0 for _ in range(other_transpose.height)] for _ in range(self.height)]
+            for i in range(self.height):
+                for j in range(other_transpose.height):
+                    multiplied_values = [x * y for x, y in zip(self.container[i], other_transpose.container[j])]
+                    val = sum(multiplied_values)
+                    result[i][j] = val
+
+        # matrix then int or float
+        elif isinstance(other, int) or isinstance(other, float):
+            result = []
+            for i in range(self.height):
+                result.append([other * val  for val in self.container[i]])
+
+        else:
+            raise MatrixInitializationError(f"Types are incompatible")   
+
+        return Matrix(result)
+
+    def __rmul__(self, other):
+        # int or float then matrix
+        result = []
+        if (isinstance(other, int) or isinstance(other, float)) and isinstance(self, Matrix):
+            for i in range(self.height):
+                result.append([other * val for val in self.container[i]])
+            return Matrix(result)
+        raise MatrixInitializationError(f"Types are incompatible")   
+
+        # transpose and inverse
+    # add an inplace parameter
+    def transpose(self):
+        if self.width == 0:
+            return Matrix([[]])
+
+        result = [[0 for _ in range(self.height)] for _ in range(self.width)]
+        for i in range(self.height):
+            for j in range(self.width):
+                result[j][i] = self.container[i][j]
+
+        return Matrix(result)
