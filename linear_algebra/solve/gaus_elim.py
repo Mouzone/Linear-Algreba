86a1da4d91fd8a7903291e78ea1b0b0c2dea3da1
Mouzone <sunnyliu010@gmail.com>
2024-02-22 20:28:32 -0500

    output of gaus_elim is now floats majority of the time, and fixed bugged solution output and episolon checker for check_unique

--- linear_algebra/solve/gaus_elim.py
+++ linear_algebra/solve/gaus_elim.py
@@ -1,4 +1,5 @@
 from linear_algebra.matrix import Matrix, MatrixInitializationError
+from linear_algebra.utility import *
 
 class Aug_matrix:
     def __init__(self, A, b):
@@ -20,13 +21,12 @@ class Aug_matrix:
             if check_upper_triangular(self):
                 break
         
-        print(self.container)
         # self.container should now be in rref form
-        self.rref = [row[:border] for row in self.container]
-        if check_unique(self): # no free variables
-            self.solution = [[row[border:] for row in self.container]]
-        else: # has free variables
-            self.solution = find_special_solutions(self)
+        self.rref = [row[:self.border] for row in self.container]
+        if check_unique(self.rref): # no free variables
+            self.solution = [row[self.border:] for row in self.container]
+        # else: # has free variables
+        #     self.solution = find_special_solutions(self)
         return 
 
 def rearrange_rows(input_matrix, start):
@@ -61,31 +61,31 @@ def reduce(input_matrix, start):
 
     return result
 
-def find_special_solutions(input_matrix):
-    input_matrix_copy = input_matrix.container.copy()
-    columns = input_matrix.A.transpose()
-    free_positions = []
-    pivot_positions = []
-    for col in range(input_matrix.border):
-        if sum(col) not in [0, 1]:
-            free_positions.append(col)
-        else:
-            pivot_positions.append(col)
+# def find_special_solutions(input_matrix):
+#     input_matrix_copy = input_matrix.container.copy()
+#     columns = input_matrix.A.transpose()
+#     free_positions = []
+#     pivot_positions = []
+#     for col in range(input_matrix.border):
+#         if sum(input_matrix.container[col]) not in [0, 1]:
+#             free_positions.append(col)
+#         else:
+#             pivot_positions.append(col)
 
-    # now we have the positions of the free columns 
-    # create the solution vecotrs by plugging values in the free positions
-    answers = [[0 for _ in range(len(input_matrix.border))] for _ in range(len(free_positions))]
-    for position, answer in zip(free_positions, answers):
-        answer[position] = 0
-        for pivot_position in pivot_positions:
-            answer[pivot_position] = 1
+#     # now we have the positions of the free columns 
+#     # create the solution vecotrs by plugging values in the free positions
+#     answers = [[0 for _ in range(input_matrix.border)] for _ in range(len(free_positions))]
+#     for position, answer in zip(free_positions, answers):
+#         answer[position] = 0
+#         for pivot_position in pivot_positions:
+#             answer[pivot_position] = 1
     
-    # now we have solution vectors for the free variables
-    # multiply each row from bottom to top whatever row sums to divide right by the left sum and set that as answer position's answer
-    # if it divides by 0 and the other value is not 0 break and let it stay as none, if both are 0 move to next row and let stay as is
-    for answer in answers:
-        for row in range(input_matrix.height-1, -1, -1):
-            b[val] - sum([answer[col] * input_matrix_copy[row][col] for col in range(len(answer))])
+#     # now we have solution vectors for the free variables
+#     # multiply each row from bottom to top whatever row sums to divide right by the left sum and set that as answer position's answer
+#     # if it divides by 0 and the other value is not 0 break and let it stay as none, if both are 0 move to next row and let stay as is
+#     for answer in answers:
+#         for row in range(input_matrix.height-1, -1, -1):
+#             b[val] - sum([answer[col] * input_matrix_copy[row][col] for col in range(len(answer))])
 
 def check_input(A, b):
     if not (isinstance(A, Matrix) or isinstance(b, Matrix)):
@@ -113,14 +113,8 @@ def check_upper_triangular(input_matrix):
 
     return True
 
-# we already know it is upper traingular, now check if singular (0 rows or free columns)
-def check_unique(input_matrix):
-    iterations = min(input_matrix.height, input_matrix.border)
-    for i in range(iterations):
-        for j in range(i, input_matrix.border):
-            if j == i and input_matrix.container[i][j] != 1:
-                return False
-            if input_matrix.container[i][j] != 0:
-                return False
-    
-    return True
+def check_unique(A, tolerance=1e-9):
+    id_matrix = identity_matrix(len(A))
+    # use epsilon function to check values against the tolerance
+    return all(all(abs(val1 - val2) < tolerance for val1, val2 in zip(row1, row2)) for row1, row2 in zip(A, id_matrix.container))
+
